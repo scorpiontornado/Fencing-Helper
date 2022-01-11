9ec624a7c7bee3c60002dcf56b47477cbb5a6b51
Nicholas Langford <40455550+scorpiontornado@users.noreply.github.com>
2022-01-11 03:32:30 +0000

    Prep work before coding allocate_poules()

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import scoresheet
 
 years = {}
 
-years["2022"] = scoresheet.Event({"school_years": "10-12", "weapon": "epee", "type": "individual"}, "name_rankings.txt")
+# TODO: create an input system to automatically add events like "2022 Jan-Mar U14 epee individual event"
 
-# Testing edge cases
-years["2022"].fencers.append(scoresheet.Fencer("John Smith", "ID1234"))
-years["2022"].fencers.append(scoresheet.Fencer("This name is quite long", "ID54321"))
+years["2022"] = [scoresheet.Event({"months": "Jan-Mar", "school_years": "10-12", "weapon": "epee", "type": "individual"}, "name_rankings.txt")]
 
-for fencer in years["2022"].fencers:
+for fencer in years["2022"][0].fencers:
   print(fencer)
 
-
 # Testing
-def generate_id(highest_id):
-  # Number of 0s to pad with
-  padding = 3 - len(str(highest_id))
-  if padding < 0: padding = 0 # padding is 0 minimum, if the number of digits is 3 or more. E.g. "ID2063"
+# def generate_id(highest_id):
+#   # Number of 0s to pad with
+#   padding = 3 - len(str(highest_id))
+#   if padding < 0: padding = 0 # padding is 0 minimum, if the number of digits is 3 or more. E.g. "ID2063"
 
-  return "ID" + "0"*padding + str(highest_id)
+#   return "ID" + "0"*padding + str(highest_id)
 
-print(f"\n{generate_id(1)} {generate_id(10)} {generate_id(100)} {generate_id(1000)} {generate_id(10000)} {generate_id(2063)}")
-# Output: ID001 ID010 ID100 ID1000 ID10000 ID2063
+# print(f"\n{generate_id(1)} {generate_id(10)} {generate_id(100)} {generate_id(1000)} {generate_id(10000)} {generate_id(2063)}")
+# # Output: ID001 ID010 ID100 ID1000 ID10000 ID2063
+
+years["2022"][0].new_round({"date":"20220111"})
--- scoresheet.py
+++ scoresheet.py
@@ -14,18 +14,22 @@ class Round:
   
   Attributes:
     + dict: metadata (contains dates the round occured over)
-    + fencer_id[]: rankings (sorted in desc. order, best to worst)
+    + fencer_id[]: prev_rankings (rankings from the previous round, sorted in desc. order, best to worst)
+    + fencer_id[]: new_rankings (rankings generated from this round, sorted in desc. order, best to worst)
     + dict: fencer_data (contains wins (w), losses (l), hits gained (hg), hits received (hr), and indicator (ind; hg - hr) for each fencer)
     + Poule[]: poules
+  
   Methods:
+    + allocate_poules() (Allocates fencers into poules based on their rankings from the previous round)
     + parse_data() (Extracts and calculates data (w, l, hg, hr, ind) for each fencer)
     + generate_rankings() (Ranks fencers in desc. order based on data from parse_data)
   '''
   # TODO: Should fencers be here or in year? Should be easy enough to change, just pass in fencers to input. But, do I want changes to fencers here to affect fencers globally?
-  def __init__(self, rankings):
-    ''' Input: rankings - list of fencers, sorted in desc. order (i.e. best to worst), from the previous round '''
-    self.rankings = rankings
-
+  def __init__(self, metadata, prev_ranks):
+    ''' Input: metadata (dict with key "date"), rankings (list of fencers, sorted in desc. order (i.e. best to worst), from the previous round) '''
+    self.metadata = metadata # TODO: should the date key be a string YYYYMMDD, DDMMYYYY, or a datetime object?
+    self.prev_ranks = prev_ranks
+    
 class Fencer:
   '''
   A single fencer, containing data for the current round only
@@ -38,7 +42,6 @@ class Fencer:
   def __init__(self, name, fencer_id):
     ''' Initiate variables '''
     self.fencer_id = fencer_id
-    
     self.name = name
 
   def __str__(self):
@@ -55,13 +58,15 @@ class Event:
     + Round[]: rounds
     
   Methods:
-    Fencer: get_fencer_by_id(fencer_id)
+    + generate_id()
+    + new_round(metadata, [id_rankings])
+    + Fencer: get_fencer_by_id(fencer_id)
   '''
     
   def __init__(self, event_data, rankings_file):
     self.event_data = event_data # including keys ("school_years" or "age_bracket"), "weapon", and "type" (individual or group)
 
-    # Read in the rankings of just the fencers' names (not their IDs) inputted by a coach into a file
+    ### Read in the rankings of just the fencers' names (not their IDs) inputted by a coach into a file
     self.fencers = []
     self.highest_id = 0 # Keeps track of the highest integer currently in use in a fencer id. First fencer will have an id of "ID001" # TODO: decide whether to pad with 0s or not # sure, makes it easier to read
     with open(rankings_file) as f:
@@ -69,8 +74,9 @@ class Event:
         self.highest_id = self.highest_id + 1 # Increment highest_id by 1
 
         self.fencers.append(Fencer(name.strip(), self.generate_id())) # Append the current name to name_rankings, stripped of newline characters
-
-    # self.rounds = [Round(self.rankings)] # Initialise the rounds with a round with hardcoded rankings (determined by one of the coaches)
+      
+    ### Initialise self.id_rankings (as self.fencers is already ordered by rank initially)
+    self.id_rankings = [fencer.fencer_id for fencer in self.fencers]
 
   def generate_id(self):
     ''' Generates a unique fencer_id, padded with zeros '''
@@ -79,3 +85,18 @@ class Event:
     if padding < 0: padding = 0 # padding is 0 minimum, if the number of digits is 3 or more. E.g. "ID2063"
   
     return "ID" + "0"*padding + str(self.highest_id)
+
+  def new_round(self, metadata, id_rankings=[]):
+    '''
+    Appends a new round to self.rounds
+    Inputs: metadata (dict with key "date"), [id_rankings]
+    id_rankings overrides the existing rankings from the previous round, e.g. if a fencer is sick for that round
+    '''
+    if id_rankings: self.id_rankings = id_rankings # override self.id_rankings if id_rankings is not empty
+    self.rounds.append(Round(metadata, self.id_rankings)) # create a new round
+
+  def get_fencer_by_id(self, fencer_id):
+    ''' Returns the fencer object associated with the given fencer_id '''
+    # Linear search
+    for fencer in self.fencers:
+      if fencer.fencer_id == fencer_id: return fencer
