5e14c309a45efb75b994256c9d19aa05a66a8660
Nicholas Langford <40455550+scorpiontornado@users.noreply.github.com>
2022-01-11 10:47:37 +0000

    Changed for loop to while loop. Untested.

--- test.py
+++ test.py
@@ -7,37 +7,40 @@ def allocate_poules(rankings, num_pools):
   poules = [[] for _ in range(num_pools)] # Create empty poules
   print(poules)
 
-  to = 0 # Poule to put the fencer in
-  direction = 1 # +1 for forwards, -1 for backwards. The number to is incremented by.]\
-  for i, fencer_id in enumerate(rankings):
-    #print("Start", to, poules[to], direction)
-    print(f"to: {to}, poules: {poules}, poules[to]: {poules[to]}, direction: {direction}")
-    #print("Middle", to, poules[to], direction, len(poules) - 1)
+  index_from = 0
+  index_to = 0 # Poule index_to put the fencer in
+  direction = 1 # +1 for forwards, -1 for backwards. The number index_to is incremented by.
+  
+  while index_from < len(rankings):
+    #print("Start", index_to, poules[index_to], direction)
+    print(f"index_to: {index_to}, poules: {poules}, poules[index_to]: {poules[index_to]}, direction: {direction}")
+    #print("Middle", index_to, poules[index_to], direction, len(poules) - 1)
 
-    poules[to].append(fencer_id)
-    if to >= len(poules) - 1:
+    poules[index_to].append(rankings[index_from])
+    if index_to >= len(poules) - 1:
       # change direction if above upper boundary, and put it back at the boundary
-      #to = len(poules) - 1
+      #index_to = len(poules) - 1
       direction = -1
       print("Now going down")
-      to += direction
-      if i != len(rankings) - 1 :
-        poules[to].append(rankings[i+1]) # TODO: make it work without this sloppy workaround
+      index_from += 1
+      if index_from != len(rankings) - 1 :
+        poules[index_to].append(rankings[index_from+1]) # TODO: make it work without this sloppy workaround
       
-    elif to <= 0 and poules[0] != [fencer_id]:
+    elif index_to <= 0 and poules[0] != rankings[index_from]:
       # The and clause prevents it from being run at the beginning.
       # change direction if below lower boundary, and put it back at the boundary
-      #to = 0 
+      #index_to = 0 
       
       direction = 1
       print("Now going up")
-      to += direction
-      if i != len(rankings) - 1 :
+      index_from += 1
+      if index_from != len(rankings) - 1 :
         # Only run if not the last fencer
-        poules[to].append(rankings[i+1]) # TODO
+        poules[index_to].append(rankings[index_from+1]) # TODO
 
-    to += direction # increment to by direction
-    #print("End", to, poules[to], direction)
+    index_from += 1
+    index_to += direction # increment index_to by direction
+    #print("End", index_to, poules[index_to], direction)
 
   return poules
 
